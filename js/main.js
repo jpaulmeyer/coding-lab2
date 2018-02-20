63783a060517b02bb346a7f455d14d00a6cc71fc
jpaulmeyer <jpaulmeyer6@gmail.com>
2018-02-20 15:50:24 -0500

    todo 1

--- js/main.js
+++ js/main.js
@@ -8,8 +8,6 @@ var instance = M.Sidenav.init(elem);
 var elem = document.querySelector('.collapsible');
 var instance = M.Collapsible.init(elem);
 
-
-
 var elem = document.querySelector('.carousel');
 var instance = M.Carousel.init(elem, {
     fullWidth: true,
